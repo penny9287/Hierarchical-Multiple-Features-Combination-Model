b26e766483e5786be88a81fab624f1ed3dc48fb7
penny9287 <21gram0807@gmail.com>
2020-06-12 21:01:47 +0800

    Add files via upload

---
+++ opennmt/decoders/__init__.py
@@ -0,0 +1,9 @@
+"""Module defining decoders."""
+
+from opennmt.decoders.rnn_decoder import RNNDecoder
+from opennmt.decoders.rnn_decoder import AttentionalRNNDecoder
+from opennmt.decoders.rnn_decoder import MultiAttentionalRNNDecoder
+from opennmt.decoders.rnn_decoder import RNMTPlusDecoder
+
+from opennmt.decoders.self_attention_decoder import SelfAttentionDecoder
+from opennmt.decoders.self_attention_decoder import SelfAttentionDecoderV2
